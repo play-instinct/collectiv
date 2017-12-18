e560499654dcad7db84871e1b441842c4ea71fa6
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2017-12-17 23:24:51 -0800

    working on signup/login forms

--- routers/user.router.js
+++ routers/user.router.js
@@ -13,3 +13,30 @@ require('../config/passport')(passport);
 
 mongoose.Promise = global.Promise;
 const {User} = require('../models/user');
+
+
+//POST request for new registration of a user to /api/user
+userRouter.post('/user', jsonParser, (req, res) => {
+  const requiredKeys = ["username", "email", "password", "user"];
+  requiredKeys.forEach( key => {
+    if(!(key in req.body)) {
+      const message = {message:`Please fill out all required fields.  Missing ${key} in request body, please try again.`};
+      return res.status(400).json(message);
+    }
+  });
+  User.create({
+    userName: req.body.username,
+    userEmail: req.body.email,
+    password: req.body.password,
+    joinDate: Date.now(),
+    avatar: req.body.avatar
+  })
+  .then(() => {
+    const message = {message:`Successfully created user ${req.body.username}`};
+    return res.status(200).json(message)
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: 'Sorry, something went wrong, please try again...'});
+  })
+});
